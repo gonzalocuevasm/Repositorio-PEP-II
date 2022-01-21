d00b0c837302166e67b79954ed6fef70dcd5149d
Gonzalo Cuevas <gonzalo.cuevas.m@usach.cl>
2022-01-21 12:31:29 -0300

    Modificaciones Eps

--- EPs-Grupo-04/EP13-respuesta-sala-4.R
+++ EPs-Grupo-04/EP13-respuesta-sala-4.R
@@ -7,37 +7,46 @@
 #        - Gonzalo Cuevas                    19.721.859-2
 #        - Daniel Jara                       20.113.716-0
 #-------------------------------------------------------------------------------
+library(pROC) 
+library(tidyr) 
+library (dplyr) 
+library (readr) 
+library ( ggpubr ) 
+library (leaps) 
+library ( car )  
+library ( caret )
+
 #-------------------------------------------------------------------------------
 # Se obtienen los datos del ejercicio anterior
 datos <- read.csv(file.choose(), head = TRUE, sep=";", encoding = "UTF-8")
 
-# Obtenci髇 de la variable IMC
+# Obtenci贸n de la variable IMC
 IMC <- datos$Weight/((datos$Height*10^-2)^2)
 
-# Obtenci髇 de la variable dicot髆ica EN dentro de datos
+# Obtenci贸n de la variable dicot贸mica EN dentro de datos
 EN <- ifelse ( IMC >= 25 , 1, 0)
 datos <- cbind ( EN , datos )
 
-# Una vez obtenido la variable dicot髆ica, ahora se realizan los siguientes
+# Una vez obtenido la variable dicot贸mica, ahora se realizan los siguientes
 # pasos.
 # 1. Recordar las ocho posibles variables predictoras seleccionadas de forma 
 #    aleatoria en el ejercicio anterior.
 
-# 2. Seleccionar, de las otras variables, una que el equipo considere que podr韆 
-#    ser 鷗il para predecir la clase EN, justificando bien esta selecci髇.
+# 2. Seleccionar, de las otras variables, una que el equipo considere que podr铆a 
+#    ser 煤til para predecir la clase EN, justificando bien esta selecci贸n.
 
-# 3. Usando el entorno R, construir un modelo de regresi髇 log韘tica con el 
+# 3. Usando el entorno R, construir un modelo de regresi贸n log铆stica con el 
 #    predictor seleccionado en el paso anterior.
 
-# 4. Usando herramientas para la exploraci髇 de modelos del entorno R, buscar 
+# 4. Usando herramientas para la exploraci贸n de modelos del entorno R, buscar 
 #    entre dos y cinco predictores de entre las variables seleccionadas al azar, 
 #    recordadas en el punto 3, para agregar al modelo obtenido en el paso. 
 
-# 5. Evaluar los modelos y "arreglarlos" en caso de que tengan alg鷑 problema 
+# 5. Evaluar los modelos y "arreglarlos" en caso de que tengan alg煤n problema 
 #    con las condiciones que deben cumplir.
 
 # 6. Evaluar el poder predictivo de los modelos en datos no utilizados para 
-#    construirlo (o utilizando validaci髇). 
+#    construirlo (o utilizando validaci贸n). 
 #-------------------------------------------------------------------------------                                                                                       cruzada) y revisar las respectivas curvas ROC.
 cat("\n----------------------------------------------------------------------")
 cat("\n                            DESARROLLO") 
@@ -66,13 +75,16 @@ cat("\n----------------------------------------------------------------------\n"
 # la cintura puede ser la variable mas representativa de las restantes, 
 # pues esta medida es comunmente utilizada para determinar el porcentaje 
 # de grasa en las personas ademas de utilizarse como un indicador para la 
-# dimensi髇 corporal de las mismas.
-# Se puede verificar dicho candidato al comparar los modelos log韘ticos ajustados 
-# completo y nulo, obtenidos a continuaci髇 mediante la funci髇 add1(), 
+# dimensi贸n corporal de las mismas.
+# Se puede verificar dicho candidato al comparar los modelos log铆sticos ajustados 
+# completo y nulo, obtenidos a continuaci贸n mediante la funci贸n add1(), 
 # efectivamente presenta el coeficiente AIC de Waist.Girth es menor que el 
-# de las dem醩 variables. 
+# de las dem谩s variables. 
 
-# Separaraci髇 de conjuntos de entrenamiento y prueba .
+#Se establece semilla utilizada en el EP anterior.
+set.seed(1274)
+# Separaraci贸n de conjuntos de entrenamiento y prueba .
+datos$EN <- factor ( datos$EN )
 n <- nrow ( datos )
 n_entrenamiento <- floor (0.8 * n )
 muestra <- sample.int ( n = n , size = n_entrenamiento , replace = FALSE )
@@ -86,16 +98,16 @@ nulo <- glm( EN ~ 1 , family = binomial ( link = "logit") , data = entrenamiento
 completo <- glm( EN ~ . , family = binomial ( link = "logit") ,
                  data = entrenamiento )
 
-cat("\n Evaluaci髇 de modelos completo y nulo mediante el AIC:\n")
+cat("\n Evaluaci贸n de modelos completo y nulo mediante el AIC:\n")
 print ( add1 ( nulo , scope = completo ) )
 cat("\n----------------------------------------------------------------------")
 cat("\n                            Paso 3") 
 cat("\n----------------------------------------------------------------------\n")
 
-# Modelo de regresi髇 log韘tica con la variable Waist.Girth
+# Modelo de regres铆on Log铆stica con la variable Waist.Girth
 modelo_WaistGirth <- glm( EN ~  Waist.Girth, family = binomial ( link = "logit") ,
                  data = entrenamiento )
-cat("\n Modelo regresi髇 log韘tica con la variable Waist.Girth")
+cat("\n Modelo regres铆on Log铆stica con la variable Waist.Girth")
 print ( summary ( modelo_WaistGirth ) )
 
 cat("\n----------------------------------------------------------------------")
@@ -103,38 +115,167 @@ cat("\n                            Paso 4")
 cat("\n----------------------------------------------------------------------\n")
 #Pruebas de modelos 
 # Predictores: Waist.Girth + Hip.Girth
-modelo2 <- glm( EM~ Waist.Girth + Hip.Girth , data = muestra )
+modelo2 <- glm( EN~ Waist.Girth + Hip.Girth , family = binomial ( link = "logit") ,
+     data = entrenamiento )
 
-AIC1 <- AIC ( modelo1)
+AIC1 <- AIC ( modelo_WaistGirth)
 AIC2 <- AIC ( modelo2)
-comparacion <- anova ( modelo1 , modelo2)
+comparacion <- anova ( modelo_WaistGirth , modelo2, test = "LRT")
+cat("\nComparaci贸n modelo 1 con modelo 2:\n")
 print(comparacion)
 
 # Se tiene que el AIC del modelo 2 es menor que el del modelo 1. al evaluar las 
-# diferencias significativas con ANOVA, se obtiene un p = 3.843e-07, inferior a 
+# diferencias significativas con ANOVA, se obtiene un p = 1.722e-13, inferior a 
 # 0.05, por lo que se concluye con un 95% de confianza, que el modelo2 es mejor 
 # que el modelo1.
 
-# Predictores: Waist.Girth + Hip.Girth + Shoulder.Girth
-modelo3 <- update( modelo2 , . ~ . + Shoulder.Girth )
+# Predictores: Waist.Girth + Hip.Girth + Thigh.Girth
+
+modelo3 <- update( modelo2 , . ~ . + Thigh.Girth )
 AIC3 <- AIC ( modelo3)
-comparacion <- anova ( modelo2 , modelo3)
+comparacion <- anova ( modelo2 , modelo3, test = "LRT")
+cat("\nComparaci贸n modelo 2 con modelo 3:\n")
 print(comparacion)
 
 # Similar al caso anterior, AIC del modelo 3 es menor que el del modelo 2, con 
-# ANOVA, p = 0.02063, por ende, se tiene un 95% de confianza en que el modelo 
-# 3 es mejor que el modelo 2
+# ANOVA, p = 1.752e-10, por ende, se tiene un 95% de confianza en que el modelo 
+# 3 es mejor que el modelo 2.
 
-# Predictores: Waist.Girth + Hip.Girth + Shoulder.Girth + Navel.Girth
-modelo4 <- update( modelo3 , . ~ . + Navel.Girth )
+# Predictores: Waist.Girth + Hip.Girth + Thigh.Girth + Bicep.Girth
+modelo4 <- update( modelo3 , . ~ . + Bicep.Girth )
 AIC4 <- AIC ( modelo4)
 comparacion <- anova ( modelo3 , modelo4)
+cat("\nComparaci贸n modelo 3 con modelo 4:\n")
 print(comparacion)
 
+# Similar al caso anterior, AIC del modelo 4 tambien es menor que el del modelo 3, con 
+# ANOVA, p = 0.02446, por ende, se tiene un 95% de confianza en que el modelo 
+# 4 es mejor que el modelo 3.
 
 cat("\n----------------------------------------------------------------------")
 cat("\n                            Paso 5") 
 cat("\n----------------------------------------------------------------------\n")
+#Requsisitos:
+
+#Verificaci贸n de Condiciones para el modelo Final: 
+
+# Generaci贸n de data.Frame con los estad铆sticos para comprobar 
+# la influencia de cada observaci贸n 
+observaciones <- data.frame ( respuesta_predicha = fitted ( modelo4 ) ) 
+observaciones [[" residuos_estandarizados "]] <- rstandard ( modelo4 ) 
+observaciones [[" residuos_estudiantizados "]] <- rstudent ( modelo4 ) 
+observaciones [[" distancia_Cook "]] <- cooks.distance ( modelo4 ) 
+observaciones [[" dfbeta "]] <- dfbeta ( modelo4 ) 
+observaciones [[" dffit "]] <- dffits ( modelo4 ) 
+observaciones [[" apalancamiento "]] <- hatvalues ( modelo4 ) 
+observaciones [[" covratio "]] <- covratio ( modelo4 ) 
+
+cat (" Identificaci贸n de valores at铆picos :\n") 
+# Observaciones con residuos estandarizados fuera del 95 % esperado . 
+atipicos <- which ( abs(observaciones [[" residuos_estandarizados "]]) > 1.96) 
+
+cat ("- Residuos estandarizados fuera del 95 % esperado :",atipicos , "\n") 
+
+# Observaciones con distancia de Cook mayor a uno. 
+distanciaCook <- which ( observaciones [[" distancia_Cook "]] > 1) 
+cat ("- Residuos con una distancia de Cook alta :",distanciaCook , "\n") 
+
+# Observaciones con apalancamiento mayor igual al doble del 
+# apalancamiento promedio . 
+apal_medio <- ( ncol ( datos ) + 1) / nrow ( datos ) 
+apalancamiento <- which ( observaciones [[" apalancamiento "]] > 2 * apal_medio ) 
+cat ("- Residuos con apalancamiento fuera de rango :",apalancamiento , "\n") 
+
+
+# Observaciones con DFBeta mayor o igual a 1. 
+dfBeta <- which ( apply ( observaciones [[" dfbeta "]] >= 1, 1 , any) ) 
+names( dfBeta ) <- NULL 
+cat ("- Residuos con DFBeta >= 1:",dfBeta , "\n") 
+
+
+
+#cat ("- Residuos con raz贸n de covarianza fuera de rango :",covarianza , "\n") 
+
+# Resumen de valores sospechosos . 
+sospechosos <- c( atipicos , distanciaCook , apalancamiento ,dfBeta) 
+sospechosos <- sort ( unique ( sospechosos ) ) 
+cat ("\ nResumen de valores sospechosos :\n") 
+cat (" Apalancamiento promedio :", apal_medio , "\n") 
+
+#cat (" Intervalo raz贸n de covarianza : [", inferior , "; ",superior , "]\n\n", sep = "") 
+
+print ( round ( observaciones [ sospechosos , c(" distancia_Cook ", " apalancamiento ") ], 2) ) 
+
+# La mayor铆a de los datos detectados como conflictivos presentan ciertas 
+# condiciones que permiten no eliminarlos de la lista, pues en alguno con 
+# con alto apalancamiento, su distancia de cook es baja o su nivel de 
+# covarianza se encuentra dentro del rango 贸ptimo. 
+
+#Verificaci贸n de las condiciones 
+# Comprobar independencia de los residuos . 
+cat (" \nPrueba de Durbin - Watson para autocorrelaciones ") 
+cat (" entre errores :\n") 
+print ( durbinWatsonTest ( modelo4 ) ) 
+
+# Con la prueba de Durbin-Watson se obtiene un p = 0.068, superior al nivel de significancia 
+# lo que nos permite afirmar con un 95% de confianza que las residuos no estan correlacionados 
+
+
+# Comprobar la multicolinealidad . 
+vifs <- vif ( modelo4 ) 
+cat ("\n Verificar la multicolinealidad :\n") 
+cat ("- VIFs :\n") 
+print ( vifs ) 
+cat ("- Tolerancias :\n") 
+print (1 / vifs ) 
+cat ("- VIF medio :", mean ( vifs ) , "\n") 
+
+# Cumple con  el criterio, todos los VIFS son menores a 5 y ademas, las toleracias 
+# son superiores a 0.2 
+
+
 cat("\n----------------------------------------------------------------------")
 cat("\n                            Paso 6") 
 cat("\n----------------------------------------------------------------------\n")
+
+# Evaluar el modelo 4 con el conjunto de entrenamiento
+cat ("\n Evaluaci贸n del modelo a partir del conjunto de entrenamiento :\n")
+probs_e <- predict ( modelo4 , entrenamiento , type = "response")
+
+umbral <- 0.5
+preds_e <- sapply ( probs_e , function (p) ifelse ( p >= umbral , "1", "0") )
+preds_e <- factor ( preds_e , levels = levels ( datos[["EN"]]) )
+
+# Se obtiene el modelo roc del modelo 4 con el conjunto de entrenamiento
+ROC_e <- roc ( entrenamiento[["EN"]] , probs_e )
+plot(ROC_e)
+
+# Matriz de confusi贸n del conjunto entrenamiento
+matriz_e <- confusionMatrix ( preds_e , entrenamiento[["EN"]])
+print ( matriz_e )
+
+# Evaluar el modelo 4 con el conjunto de prueba .
+cat (" Evaluaci贸n del modelo a partir del conjunto de prueba :\n")
+probs_p <- predict ( modelo4 , prueba , type = "response")
+
+preds_p <- sapply ( probs_p , function ( p ) ifelse ( p >= umbral , "1", "0") )
+preds_p <- factor ( preds_p , levels = levels ( datos [["EN"]]) )
+
+# Se obtiene el modelo roc del modelo 4 con el conjunto de prueba
+ROC_p <- roc ( prueba[["EN"]] , probs_p )
+plot ( ROC_p )
+
+# Matriz de confusi贸n del conjunto de prueba
+matriz_p <- confusionMatrix ( preds_p , prueba[["EN"]])
+print ( matriz_p )
+
+# Conclusi贸n:
+# La curva de ROC vista en los datos de entrenamiento y de prueba se aleja bastante
+# de la diagonal, indicando que existe una gran precisi贸n, causando que este sea un 
+# buen modelo.
+# Del mismo modo, la matriz de confusi贸n para el conjunto de entrenamiento y de 
+# puebas nos indica que, estos poseen un nivel de exactitud del 91% y 82% respectivamente,
+# a su vez, la sensitividad de cada uno es del 95% y 92% y su especifidad del 80% y 60%
+# indicando que  existe la posibilidad de que el conjunto de pruebas sea demasiado peque帽o
+# para poder comprobar los valores correctamente, pues se detecta que existe una diferencia
+# en la exactitud de ambos conjuntos.
